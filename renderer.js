8bb3cf38e0c1d6b6fd05babdd7d60628c88fc157
YaBoiAsh <nawabaarij@gmail.com>
2023-04-21 01:22:51 +0500

    login pane added

--- renderer.js
+++ renderer.js
@@ -2,7 +2,14 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  document.getElementById('login').addEventListener('click', () => {
+  // document.getElementById('login').addEventListener('click', () => {
+  //   console.log('login button pressed');
+  //   window.api.send('login-success');
+  // });
+  const submitFormButton = document.querySelector("#login-panel");
+
+  submitFormButton.addEventListener("login", function(event){
+    event.preventDefault();
     console.log('login button pressed');
     window.api.send('login-success');
   });
