81412649302c3bd320e016c06e332040f9863ca4
aarijimam <35100854+aarijimam@users.noreply.github.com>
2023-04-28 03:32:27 +0500

    Cleaned Up Code and Errors implemented

--- preload.js
+++ preload.js
@@ -1,15 +1,12 @@
-const {
-  contextBridge,
-  ipcRenderer
-} = require("electron");
+const {contextBridge, ipcRenderer } = require("electron");
+const toastify = require ('toastify-js');
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld(
-  "api", {
-      send: (data) => {
-              ipcRenderer.send(data);
-      },
-     
-  }
-);
+contextBridge.exposeInMainWorld("api",{
+      send:(data) => ipcRenderer.send(data)
+});
+
+contextBridge.exposeInMainWorld('toastify',{
+  toast:(options) => toastify(options).showToast()
+})
--- renderer.js
+++ renderer.js
@@ -1,10 +1,9 @@
 
 const usernamelogin = "admin";
 const passwordlogin = "123";
+const form = document.getElementById('login-panel')
 
-document.addEventListener('DOMContentLoaded', () => {
-
-  document.getElementById('login-panel').addEventListener('submit', (event) => {
+function formSubmitted(event){
     // Prevent the form from actually being "submitted", as that will effectively reload the page.
     event.preventDefault()
 
@@ -16,10 +15,37 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log([username, password]);
 
     if(username == usernamelogin && password == passwordlogin){
+      alertMessage('login successful')
       window.api.send('login-success');
+    } else {
+      errorMessage('Incorrect Username or Password!');
     }
+}
 
-
+function alertMessage(message){
+  toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style:{
+      background: 'green',
+      color: 'white',
+      textAlign: 'center'
+    }
   })
+}
 
-});
+function errorMessage(message){
+  toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style:{
+      background: 'red',
+      color: 'white',
+      textAlign: 'center'
+    }
+  })
+}
+
+form.addEventListener('submit', formSubmitted)
