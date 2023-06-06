f610e309fa16c2fe6d12ee6febaf8b629f8907c4
Muhammad-Ali <107263899+codingInEME@users.noreply.github.com>
2023-06-07 03:09:44 +0500

    fixed round robin
    fixed round robin gantt chart
    fixed the output of round robin

--- preload.js
+++ preload.js
@@ -1,15 +1,28 @@
-const {
-  contextBridge,
-  ipcRenderer
-} = require("electron");
+//const { Chart } = require('chart.js')
+//import Chart from 'chart.js/auto';
+const { contextBridge, ipcRenderer } = require("electron")
 
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld(
-  "api", {
-      send: (data) => {
-              ipcRenderer.send(data);
-      },
-     
-  }
-);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  })
+
+/* contextBridge.exposeInMainWorld('Chart', {
+  chartCreate: (ctx, data) => Chart(ctx, data),
+  chartDelete: () => Chart.clear()
+}) */
+
+contextBridge.exposeInMainWorld('api',{
+    login_press: () => ipcRenderer.invoke('login_press'),
+    send: (data) => ipcRenderer.send(data),
+    minimize: () => ipcRenderer.invoke('minimize')
+
+})
+
--- renderer.js
+++ renderer.js
@@ -1,25 +1,15 @@
 
-const usernamelogin = "admin";
-const passwordlogin = "123";
-
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('quit-button').addEventListener('click', () => {
+        console.log("APP QUIT")
+        window.api.send('quit_app')
+    })
 
-  document.getElementById('login-panel').addEventListener('submit', (event) => {
-    // Prevent the form from actually being "submitted", as that will effectively reload the page.
-    event.preventDefault()
-
-    // event.target refers to the <form> element that's attached to the submit event.
-    const form = event.target
-    const username = form.username.value;
-    const password = form.password.value;
-
-    console.log([username, password]);
-
-    if(username == usernamelogin && password == passwordlogin){
-      window.api.send('login-success');
-    }
+    document.getElementById('login').addEventListener('click', () => {
+        window.close()
+        window.api.login_press()
+    })
 
 
-  })
-
-});
+    //document.querySelector('#login').addEventListener()
+})
